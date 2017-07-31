0d42caf8873d4e618ed646e05830756ff2f5efe0
Adi Iyengar <aditya7iyengar@gmail.com>
2017-07-31 08:40:13 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -2,13 +2,15 @@ defmodule Cerberus.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :cerberus,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     elixirc_paths: elixirc_paths(Mix.env),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :cerberus,
+      version: "0.2.0",
+      elixir: "~> 1.4",
+      elixirc_paths: elixirc_paths(Mix.env),
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
