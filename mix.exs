314f48da409d336930b349f612b04280e1cbb9e9
Adi Iyengar <aditya7iyengar@gmail.com>
2017-07-31 08:37:39 -0400

    Bump elixir, plug and mime:
    - Bump elixir version to 1.4.2
    - Plug to 1.4.3
    - Mime to 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,21 +4,23 @@ defmodule Cerberus.Mixfile do
   def project do
     [app: :cerberus,
      version: "0.1.0",
-     elixir: "~> 1.3",
-     # elixirc_paths: elixirc_paths(Mix.env),
+     elixir: "~> 1.4",
+     elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
   end
 
-  # defp elixirc_paths(:test), do: ["lib", "test/support"]
-  # defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
 
   def application do
     [applications: [:logger]]
   end
 
   defp deps do
-    [{:plug, "~> 1.0"}]
+    [
+      {:plug, "~> 1.0"}
+    ]
   end
 end
