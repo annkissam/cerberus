927a1ba33980ed25b8362781fb8eca0a799bac00
Adi Iyengar <aditya7iyengar@gmail.com>
2017-10-08 09:54:00 -0400

    Make policy module more explicit

--- lib/cerberus.ex
+++ lib/cerberus.ex
@@ -10,11 +10,21 @@ defmodule Cerberus do
     |> apply(:sanction, [user, action, resource])
   end
 
+  @spec sanctioned?(user :: any, action :: atom, resource :: any) :: boolean
+  def sanctioned?(mod, user, action, resource) do
+    apply(mod, :sanction, [user, action, resource])
+  end
+
   @spec auth_scope(user :: any, scope :: any, action :: atom) :: any
   def auth_scope(user, scope, action) do
     scope
     |> fetch_policy_module
     |> apply(:scope, [user, scope, action])
+  end
+
+  @spec auth_scope(user :: any, scope :: any, action :: atom) :: any
+  def auth_scope(mod, user, scope, action) do
+    apply(mod, :scope, [user, scope, action])
   end
 
   @spec fetch_policy_module(any) :: module | :error
