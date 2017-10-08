358c93ce40bc8cf00e9cd5efbf35f941f03a8c68
Adi Iyengar <aditya7iyengar@gmail.com>
2017-10-08 09:50:58 -0400

    Clean up

--- lib/cerberus/controller.ex
+++ lib/cerberus/controller.ex
@@ -3,27 +3,29 @@ defmodule Cerberus.Controller do
   Wraps controllers with Cerberus authorization methods.
   """
 
+  alias Plug.Conn
+
   @doc """
   Authorizes a resource, for a user, for a given action, then marks the connection as having had cerberus_auth_perfomed
   """
   @spec authorize(
-    conn :: Plug.Conn.t,
+    conn :: Conn.t,
     user :: any,
     resource :: any,
     action :: atom
-  ) :: Plug.Conn.t
+  ) :: Conn.t
   def authorize(conn, user, resource, action) do
     conn
     |> perform_auth(user, resource, action)
-    |> Plug.Conn.put_private(:cerberus_auth_performed, true)
+    |> Conn.put_private(:cerberus_auth_performed, true)
   end
 
   @spec perform_auth(
-    conn :: Plug.Conn.t,
+    conn :: Conn.t,
     user :: any,
     resource :: any,
     action :: atom
-  ) :: Plug.Conn.t
+  ) :: Conn.t
   defp perform_auth(conn, user, resource, action) do
     if Cerberus.sanctioned?(user, action, resource) do
       conn
@@ -40,5 +42,7 @@ defmodule Cerberus.Controller do
     scope :: any,
     action :: atom
   ) :: any
-  def auth_scope(user, scope, action), do: Cerberus.auth_scope(user, scope, action)
+  def auth_scope(user, scope, action) do
+    Cerberus.auth_scope(user, scope, action)
+  end
 end
